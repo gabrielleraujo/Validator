57c7cabbfa635b21b72e8c55a762bab3eb369935
unknown <gabrielleraujo@gmail.com>
2021-10-31 12:50:44 -0300

    refactoring

--- Validator.Tests/CelularValidationTest.cs
+++ Validator.Tests/CelularValidationTest.cs
@@ -6,16 +6,19 @@ namespace Validator.Tests
     using Xunit;
 
 
-    public class TelefoneValidationTest
+    public class CelularValidationTest
     {
+        const string validMessage = "Número de celular válido";
+        const string invalidMessage = "Número de celular inválido";
+
         private void Test(string expectedResult, string telefone)
         {
             //arrange
-            var logger = MyLoggerFactory.Create<TelefoneValidationTest>();
+            var logger = MyLoggerFactory.Create<CelularValidationTest>();
            
             ICellphoneValidation validator = new CellphoneValidation(logger);
 
-            string result = "Número de telefone válido";
+            string result = validMessage;
 
             //act
             try
@@ -33,25 +36,24 @@ namespace Validator.Tests
 
 
         [Theory]
-        [InlineData("Número de telefone inválido", "(03) 99010-0070")]
-        [InlineData("Número de telefone inválido", "(33) 29900-8000")]
-        [InlineData("Número de telefone inválido", "(00) 00000-0000")]
-        [InlineData("Número de telefone inválido", "(21) 9900-0002")]
-        [InlineData("Número de telefone inválido", "21 79900-8070")]
-        [InlineData("Número de telefone inválido", "(88) 00000-0000")]
+        [InlineData(invalidMessage, "(03) 99010-0070")]
+        [InlineData(invalidMessage, "(33) 29900-8000")]
+        [InlineData(invalidMessage, "(00) 00000-0000")]
+        [InlineData(invalidMessage, "(21) 9900-0002")]
+        [InlineData(invalidMessage, "21 79900-8070")]
+        [InlineData(invalidMessage, "(88) 00000-0000")]
         public void ValidateReturnsInvalidWhenPhoneNumberIsInvalid(string resultadoEsperado, string telefone)
         {
             Test(resultadoEsperado, telefone);
         }
 
-        [Theory]
-        [InlineData("Número de telefone válido", "(88) 97845-8070")]
-        [InlineData("Número de telefone válido", "(12) 97925-8552")]
-        [InlineData("Número de telefone válido", "21971573847")]
-        [InlineData("Número de telefone válido", "8297845-8070")]
-        [InlineData("Número de telefone válido", "12 7157-3847")]
-        
 
+        [Theory]
+        [InlineData(validMessage, "(88) 97845-8070")]
+        [InlineData(validMessage, "(12) 97925-8552")]
+        [InlineData(validMessage, "21971573847")]
+        [InlineData(validMessage, "8297845-8070")]
+        [InlineData(validMessage, "12 7157-3847")]
         public void ValidateReturnsValidWhenPhoneNumberIsValid(string resultadoEsperado, string telefone)
         {
             Test(resultadoEsperado, telefone);
--- Validator/Validations/CellphoneValidation.cs
+++ Validator/Validations/CellphoneValidation.cs
@@ -7,23 +7,26 @@
 
     public class CellphoneValidation : ICellphoneValidation
     {
+        const string validMessage = "Número de celular válido";
+        const string invalidMessage = "Número de celular inválido";
+
         private string _pattern = "^\\(?(?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\\)? ?(?:[2-8]|9[1-9])[0-9]{3}\\-?[0-9]{4}$";
         
         private ILogger _logger;
         public CellphoneValidation(ILogger logger) { _logger = logger; }
 
 
-        public void Validate(string telefone)
+        public void Validate(string cellphone)
         {
-            var result = Regex.Match(telefone, _pattern);
+            var result = Regex.Match(cellphone, _pattern);
 
             if (!result.Success)
             {
-                _logger.LogError("Número de telefone inválido");
+                _logger.LogError(invalidMessage);
 
-                throw new ArgumentException("Número de telefone inválido");
+                throw new ArgumentException(invalidMessage);
             }
-            _logger.LogInformation("Número de telefone válido");
+            _logger.LogInformation(validMessage);
         }
     }
 }
